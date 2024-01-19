47e830475d385bb8aa6318801e3404f785390c6a
Luca Arduini <arduini37@gmail.com>
2024-01-19 21:33:40 +0100

    test

--- BeatBuddy/src/main/java/it/unipi/lsmd/BeatBuddy/DTO/Album_DTO.java
+++ BeatBuddy/src/main/java/it/unipi/lsmd/BeatBuddy/DTO/Album_DTO.java
@@ -1,4 +1,17 @@
 package it.unipi.lsmd.BeatBuddy.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
 public class Album_DTO {
+    @Id
+    private String id;
+    private String name;
+    private String[] artists;
+    private String coverUrl;
+
+
 }
