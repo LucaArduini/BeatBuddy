4dd8c4081969fc9e64cf8f0a83d6fc3ba06430ee
Luca Arduini <arduini37@gmail.com>
2024-01-26 11:57:16 +0100

    forse_sto-riparando

---
+++ BeatBuddy/src/main/java/it/unipi/lsmd/BeatBuddy/model/dummy/AlbumLikes.java
@@ -0,0 +1,11 @@
+package it.unipi.lsmd.BeatBuddy.model.dummy;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlbumLikes {
+    private String coverURL;
+    private Integer likes;
+}
